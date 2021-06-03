88c2822d68d59a59e4ac494c2f896b6c051c0d02
Isaias Fernandez <isafernande04@gmail.com>
2021-06-03 11:55:17 -0300

    Logicas de entradas y salidads de texto listas, falta logica de continuacion de textos

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-alert('Hola, como estas?');
+/* alert('Hola, como estas?');
 alert('Desde hace mucho eh querido preguntarte esto');
 alert('Me ah costado mucho valor decirte esto, pero...');
-alert('Te gustaria salir en una cita conmigo?');
+alert('Te gustaria salir en una cita conmigo?'); */
 
-let btnYes = document.getElementById('btn-yes');
-let btnNo = document.getElementById('btn-no');
-let container = document.getElementById('container');
-
-btnNo.addEventListener('mousemove', hadlerBtnNo);
-btnYes.addEventListener('click', hadlerBtnYes);
-
-if (screen.width === 500) {
-  btnNo.removeEventListener('mousemove', hadlerBtn);
-  btnNo.addEventListener('click', hadlerBtn);
-}
+let btnYes = document.querySelector('#btn-yes');
+let btnNo = document.querySelector('#btn-no');
+let container = document.querySelector('#container');
+let introContainer = document.querySelector('.intro');
+let placeHolder = document.querySelector('h1');
 
 let x;
 let y;
@@ -38,7 +32,52 @@ function hadlerBtnNo() {
 }
 
 function positionRandom() {
-  x = Math.floor(Math.random() * (maxSpaceWidht - 0 + 1) + 0);
-  y = Math.floor(Math.random() * (maxSpaceHeight - 0 + 1) + 0);
+  x = Math.floor(Math.random() * maxSpaceWidht);
+  y = Math.floor(Math.random() * maxSpaceHeight);
   return x, y;
+}
+
+function isMobile(){
+  return (
+      (navigator.userAgent.match(/Android/i)) ||
+      (navigator.userAgent.match(/webOS/i)) ||
+      (navigator.userAgent.match(/iPhone/i)) ||
+      (navigator.userAgent.match(/iPod/i)) ||
+      (navigator.userAgent.match(/iPad/i)) ||
+      (navigator.userAgent.match(/BlackBerry/i))
+  );
+}
+function oscurecer(elemento){
+  elemento.style.opacity=0;
+}
+function aparecer(elemento){
+  elemento.style.opacity=1;
+}
+function desaparecerIntro(){
+  introContainer.style.display = 'none';
+}
+function agregarTexto(){
+  placeHolder.append('Hace un tiempo quería decirte esto..')
+  placeHolder.style.opacity=1
+  setTimeout(eliminarTexto,2000)
+}
+function eliminarTexto(){
+  oscurecer(placeHolder)
+  /* oscurecer(introContainer) */
+  setTimeout(desaparecerIntro,3000)
+}
+function intro(){
+  introContainer.style.display = 'flex';
+  setTimeout(agregarTexto,1000)
+}
+
+
+window.addEventListener('load',intro)
+
+btnNo.addEventListener('mousemove', hadlerBtnNo);
+btnYes.addEventListener('click', hadlerBtnYes);
+
+if (isMobile()) {
+  btnNo.addEventListener('mousemove', hadlerBtnNo);
+  btnYes.addEventListener('click', hadlerBtnYes);
 }
